ed4dad48aea2f7d7b427bbf7f2c09a414c80a3cc
kishimoto-banana <drehbleistift@gmail.com>
2020-01-05 21:05:12 +0900

    Fix entrypoint

--- crawler/entrypoint.sh
+++ crawler/entrypoint.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-# wait-for-postgres.sh
-
-set -e
-
-host="$1"
-shift
-cmd="$@"
-
-until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - executing command"
-#exec $cmd
-
 cd /crawler/database
 orator migrate -f
+
 cd /crawler/hatena_crawler
+scrapy crawl hatena_crawler
